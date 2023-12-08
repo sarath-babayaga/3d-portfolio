860b16351724709e17c46f485806e12d754b150b
portfolio <sarathblu1996@gmail.com>
2023-12-08 10:18:43 +0530

    added details

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
               <>
                 <Routes>
                   <Route path='/about' element={<About />} />
-                  <Route path='/projects' element={<Projects />} />
+                  {/* <Route path='/projects' element={<Projects />} /> */}
                   <Route path='/contact' element={<Contact />} />
                 </Routes>
                 {/* <Footer /> */}
